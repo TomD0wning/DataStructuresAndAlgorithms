e7b61d2b6fa57db54edd95772c33c93421f292d0
Tom Downing <tom_downin92@hotmail.com>
2022-09-15 19:28:36 +0100

    removed unused files and added to sample code

--- Classes.py
+++ Classes.py
@@ -1,11 +1,12 @@
 # ! /usr/bin/env python3
 
+
 class Money:
 
-# __init__ is the name of  the constructor
-#  (self,anAmount, aCurrency) are the arguments pass to the constructor
-# self refers to an object under construction
-    def __init__(self,anAmount, aCurrency):
+    # __init__ is the name of  the constructor
+    #  (self,anAmount, aCurrency) are the arguments pass to the constructor
+    # self refers to an object under construction
+    def __init__(self, anAmount, aCurrency):
         self.amount = anAmount
         self.currency = aCurrency
 
@@ -14,23 +15,23 @@ class Money:
 
     def __add__(self, otherMoney):
         if self.currency == otherMoney.currency:
-            return  Money((self.amount + otherMoney.amount),self.currency)
+            return Money((self.amount + otherMoney.amount), self.currency)
 
     def convert(self, rate, newCurrency):
         newAmount = self.amount*rate
-        self.amount = round(newAmount,2)
+        self.amount = round(newAmount, 2)
         self.currency = newCurrency
         return self
-    
-    def times(self,factor):
-        return Money((self.amount * factor),self.currency)
+
+    def times(self, factor):
+        return Money((self.amount * factor), self.currency)
 
 
-money1 = Money(100,'GBP')
-print(money1.amount,money1.currency)
+money1 = Money(100, 'GBP')
+print(money1.amount, money1.currency)
 
-money2 = Money(50,'EUR')
-money3 = money2.convert(3,'RUB')
+money2 = Money(50, 'EUR')
+money3 = money2.convert(3, 'RUB')
 print(money3)
 
 money4 = Money(11, 'EUR')
--- ColourSortingOnWaveLength.py
+++ ColourSortingOnWaveLength.py
@@ -1,20 +1,21 @@
 import time
 
-#Implementation of various sorting algorithms that sort colours based on the RGB to wavelength conversion
+# Implementation of various sorting algorithms that sort colours based on the RGB to wavelength conversion
 
 
-# inseratoin sort, takes a wavelength and sorts the corresponding pixles
+# insertion sort, takes a wavelength and sorts the corresponding pixels
 def iterationSort(aList):
     start = time.time()
-    for index in range(1,len(aList)):
+    for index in range(1, len(aList)):
         currentValue = aList[index]
         position = index
 
-        while position>0 and aList[position-1]>currentValue:
-            aList[position]=aList[position-1]
+        while position > 0 and aList[position-1] > currentValue:
+            aList[position] = aList[position-1]
             position = position-1
-        aList[position]=currentValue
+        aList[position] = currentValue
     end = time.time()
     return aList, end-start
 
-print("IterationSort ",iterationSort(aList))
+
+print("IterationSort ", iterationSort(aList))
