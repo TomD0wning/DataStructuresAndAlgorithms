b3ab4e745f389388f26d3800431b276f0b86995b
Tom Downing <tom_downin92@hotmail.com>
2023-01-04 18:16:25 +0000

    couple of dojo challenges

--- Dojo.py
+++ Dojo.py
@@ -1,15 +1,37 @@
 #! /usr/bin/env python3
 
-def naturalNumbers():
-    print(sum(_ for _ in range(0,1000) if( _ % 3 == 0 or _ % 5 ==0)))
+import collections
 
-# naturalNumbers
+def natural_numbers():
+    print(sum(_ for _ in range(0, 1000) if(_ % 3 == 0 or _ % 5 == 0)))
 
 
-def isTrue():
-    a = False
-    b = True
-    if(a or not b):
-        print("here")
+def valid_parentheses(input):
+    count = [0]
+    [count.append(1) if x is '(' else count.pop()
+     if x is ')' and count != [] else '' for x in input]
+    return len(count) == 1
 
-isTrue()
+
+def hashtag_gen(s):
+    result = s.replace(" ", "")
+    if(len(result) > 139 or len(result) == 0):
+        return False
+    return f"#{result.title()}"
+
+
+def first_non_repeating_letter(s):
+    res = collections.Counter(s)
+    return min(res, key=res.get)
+
+
+def offset_weights(s):
+    lst = list(s.split(" "))
+    result = []
+    for x in lst:
+        result.append((x, sum(int(i) for i in x)))
+    return [n[0] for n in sorted(result, key=lambda s: s[1])]
+
+
+def ends_with(s: str, e: str):
+    return s.endswith(e)
